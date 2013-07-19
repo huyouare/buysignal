4ab6ae4450742be533d0c1d1f6f49a82dc1c070a
Jesse Hu <huyouare@gmail.com>
2013-07-18 22:42:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buysignal::Application.config.secret_token = '75091b03ead3d60b5f96fcd670f7d29dddf15ab253586d3b0850a8b1fc8d412258044f319986f3af5bac80f7cd87b8ecf15bf14f50443b516fdea632dd0d8fa5'
