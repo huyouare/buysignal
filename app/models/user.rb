2adb197bebe42c07d1d191194648c53aa7252cf3
Jesse Hu <huyouare@gmail.com>
2013-07-19 18:40:50 -0400

    Fix User Models with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  
 
+  validates_presence_of :name
+
+  # Setup accessible (or protected) attributes for your model
+  # attr_accessible :title, :body
+	attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
