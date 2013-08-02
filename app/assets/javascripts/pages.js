85ac70b846daf45245d48683c5d992d06b07649b
Jesse Hu <huyouare@gmail.com>
2013-08-02 03:10:27 -0400

    Fixed slider one last time

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -20,15 +20,23 @@ $(window).ready(function () {
         $('#myCarousel .slide-2 .carousel-caption form').removeClass('slide-in');
       }
       if($('#myCarousel .active').hasClass('slide-2')){
-        $('#myCarousel .slide-1 .carousel-caption form').removeClass('slide-in');
-        $('#myCarousel .slide-2 .carousel-caption form').removeClass('slide-in');
+        $('#myCarousel .slide-1').addClass('left-click');
+        $('#myCarousel .slide-2').addClass('left-click');
+        $('#myCarousel .slide-1 .carousel-caption form').addClass('slide-in');
+        $('#myCarousel .slide-2 .carousel-caption form').addClass('slide-in');
       }
     });
 
     $('#myCarousel').on('slide', function(){
-        if($('#myCarousel .active').hasClass('slide-2')){            
-          $('#myCarousel .slide-1 .carousel-caption form').toggleClass('slide-in');
-          $('#myCarousel .slide-2 .carousel-caption form').toggleClass('slide-in');
+        if($('#myCarousel .active').hasClass('slide-2')){
+          if($('#myCarousel .active').hasClass('left-click')){  
+            $('#myCarousel .slide-1').removeClass('left-click');
+            $('#myCarousel .slide-2').removeClass('left-click');
+          }
+          else {          
+            $('#myCarousel .slide-1 .carousel-caption form').removeClass('slide-in');
+            $('#myCarousel .slide-2 .carousel-caption form').removeClass('slide-in');
+          }
         }
         // if($('#myCarousel .active').hasClass('slide-1')){            
         //   $('#myCarousel .slide-1 .carousel-caption form').toggleClass('slide-in');
